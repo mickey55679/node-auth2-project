d9e721a79883eb6745d6dc3ec329d5c9ab5fb889
Michaiah Bos <michaiahbos@yahoo.com>
2024-04-09 16:10:17 -0500

    wip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,5 +1,6 @@
 const { JWT_SECRET } = require("../secrets"); // use this secret!
 const { findBy } = require("../users/users-model");
+const jwt = require("jsonwebtoken");
 
 const restricted = (req, res, next) => {
   /*
@@ -17,7 +18,18 @@ const restricted = (req, res, next) => {
 
     Put the decoded token in the req object, to make life easier for middlewares downstream!
   */
-  next();
+  const token = req.headers.authorization;
+  if (!token) {
+    return next({ status: 401, message: "Token required" });
+  }
+  jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+    if (err) {
+      next({status: 401, message: 'Token invalid'})
+    } else {
+    req.decodedToken = decodedToken
+    next()
+    }
+  });
 };
 
 const only = (role_name) => (req, res, next) => {
@@ -31,7 +43,7 @@ const only = (role_name) => (req, res, next) => {
 
     Pull the decoded token from the req object, to avoid verifying it again!
   */
-  next();
+ 
 };
 
 const checkUsernameExists = async (req, res, next) => {
@@ -47,8 +59,8 @@ const checkUsernameExists = async (req, res, next) => {
     if (!user) {
       next({ status: 422, message: "Invalid credentials" });
     } else {
-     req.user = user
-     next()
+      req.user = user;
+      next();
     }
   } catch (err) {
     next(err);
@@ -75,15 +87,15 @@ const validateRoleName = (req, res, next) => {
     }
   */
   if (!req.body.role_name || !req.body.role_name.trim()) {
-    req.role_name = "student"
-   next()
+    req.role_name = "student";
+    next();
   } else if (req.body.role_name.trim() === "admin") {
-    next({ status: 422, message: "Role name can not be admin" })
+    next({ status: 422, message: "Role name can not be admin" });
   } else if (req.body.role_name.trim().length > 32) {
-    next({ status: 422, message: "Role name can not be longer than 32 chars" })
+    next({ status: 422, message: "Role name can not be longer than 32 chars" });
   } else {
-    req.role_name = req.body.role_name.trim()
-    next()
+    req.role_name = req.body.role_name.trim();
+    next();
   }
 };
 
